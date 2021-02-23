fb29acd876e34aad25bdb36a76db95f0e8b65550
DavidCastillo2 <58519904+DavidCastillo2@users.noreply.github.com>
2021-02-22 21:22:15 -0500

    Fixed Weirdness

--- GameProject1/OurCode/OurAI.java
+++ GameProject1/OurCode/OurAI.java
@@ -7,17 +7,15 @@ import java.util.Random;
 public class OurAI implements Player {
 
     public Move getMove(GameState state){
+        return chooseMove(state);
+    }
 
+    // Currently Chooses a random move
+    public Move chooseMove(GameState state) {
         Random rand = new Random();
         boolean done = false;
         PlayerID cur_player = state.getCurPlayer();
         Move rand_move = null;
-
-        int bin1 = state.getStones(cur_player, 1);
-        int no1 = state.getStones(cur_player, 1);
-        System.out.println(bin1);
-        System.out.println(no1);
-        System.out.println("");
 
         while ( ! done ){
             int bin = rand.nextInt(6);
