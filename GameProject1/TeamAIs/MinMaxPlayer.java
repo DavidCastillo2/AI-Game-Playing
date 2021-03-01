045f92a42488b2703aef678382b3583814889843
DavidCastillo2 <58519904+DavidCastillo2@users.noreply.github.com>
2021-03-01 03:17:52 -0500

    Completed Austin's code to have a working MinMax method

---
+++ GameProject1/TeamAIs/MinMaxPlayer.java
@@ -0,0 +1,29 @@
+package TeamAIs;
+
+import MiniMax.Minimax;
+import ProjectOneEngine.*;
+
+import java.util.Random;
+
+public class MinMaxPlayer implements Player {
+
+    public Move getMove(GameState state){
+
+        boolean done = false;
+        Move move = null;
+
+        Minimax Minimax = new Minimax(state.getCurPlayer());
+        while (!done){
+            move = Minimax.findMove(state, 9);
+            if (GameRules.makeMove(state, move) != null){
+                done = true;
+            }
+        }
+        System.out.println("");  // Just to help make it clear what move was made
+        return move;
+    }
+
+    public String getPlayName(){
+        return "MinMax Player";
+    }
+}
